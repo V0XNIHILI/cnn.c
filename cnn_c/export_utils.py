85f4089d9a65bb4752d58dcee61108bf28aae523
Douwe den Blanken <douwedb@gmail.com>
2024-03-10 01:04:22 +0100

    Make export a simple .flatten() call

--- cnn_c/export_utils.py
+++ cnn_c/export_utils.py
@@ -2,4 +2,4 @@ import numpy as np
 
 def save_tensor_to_file(filename: str, tensor: np.ndarray):
     with open(filename, 'wb') as file:
-        tensor.T.flatten().astype(np.float32).tofile(file)
+        tensor.flatten().astype(np.float32).tofile(file)
