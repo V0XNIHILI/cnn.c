20c55865920b518612155419c609edf206cb6880
Timdnb <timdnblanken@gmail.com>
2024-03-13 16:46:48 +0100

    Improve indexing for conv_2d and max_pool_2d

--- src/nn.c
+++ src/nn.c
@@ -78,34 +78,49 @@ Tensor *conv_2d(const Tensor *input, const Tensor *weight, const Tensor *bias, s
     size_t output_dims[] = {batch_size, output_channels, output_height, output_width};
     Tensor *output = create_tensor(4, output_dims);
 
+    size_t weight_index, wi_output_channels, wi_input_channels, wi_kernel_height, wi_kernel_width;
+    size_t input_index, ii_batch_size, ii_input_channels, ii_input_height, ii_input_width;
+    size_t output_index, oi_batch_size, oi_output_channels, oi_output_height, oi_output_width;
+
     size_t i;
-    #pragma omp parallel for private (i)
+    #pragma omp parallel for private(i, weight_index, wi_output_channels, wi_input_channels, wi_kernel_height, wi_kernel_width, input_index, ii_batch_size, ii_input_channels, ii_input_height, ii_input_width, output_index, oi_batch_size, oi_output_channels, oi_output_height, oi_output_width)
     for (i = 0; i < output_channels; i++) {
+        wi_output_channels = i * input_channels * kernel_height * kernel_width;
+        oi_output_channels = i * output_height * output_width;
         for (size_t n = 0; n < input_channels; n++) {
+            wi_input_channels = n * kernel_height * kernel_width;
+            ii_input_channels = n * input_height * input_width;
             for (size_t l = 0; l < kernel_height; l++) {
+                wi_kernel_height = l * kernel_width;
                 for (size_t m = 0; m < kernel_width; m++) {
-                    float current_weight = get_tensor_entry_value(weight, (size_t[]) {i, n, l, m});
+                    wi_kernel_width = m;
+                    weight_index = wi_output_channels + wi_input_channels + wi_kernel_height + wi_kernel_width;
+
+                    float current_weight = weight->data[weight_index];
+
                     bool set_bias = n == 0 && l == 0 && m == 0;
 
                     for (size_t b = 0; b < batch_size; b++) {
+                        oi_batch_size = b * output_channels * output_height * output_width;
+                        ii_batch_size = b * input_channels * input_height * input_width;
                         for (size_t j = 0; j < output_height; j++) {
+                            oi_output_height = j * output_width;
+                            ii_input_height = (j * stride + l) * input_width;
                             for (size_t k = 0; k < output_width; k++) {
-                                size_t current_input_indices[4];
+                                oi_output_width = k;
+                                output_index = oi_batch_size + oi_output_channels + oi_output_height + oi_output_width;
 
-                                current_input_indices[0] = b;
-                                current_input_indices[0+has_batch_dim] = n;
-                                current_input_indices[1+has_batch_dim] = j * stride + l;
-                                current_input_indices[2+has_batch_dim] = k * stride + m;
+                                ii_input_width = k * stride + m;
+                                input_index = ii_batch_size + ii_input_channels + ii_input_height + ii_input_width;
 
-                                size_t current_output_index = get_tensor_entry_index(output, (size_t[]) {b, i, j, k});
-
-                                float new_value = get_tensor_entry_value(input, current_input_indices) * current_weight;
+                                float new_value = input->data[input_index] * current_weight;
 
                                 if (set_bias) {
-                                    output->data[current_output_index] = new_value + bias->data[i];
-                                } else {
-                                    output->data[current_output_index] += new_value;
-                                }                                
+                                    output->data[output_index] = new_value + bias->data[i];
+                                }
+                                else {
+                                    output->data[output_index] += new_value;
+                                }
                             }
                         }
                     }
@@ -126,9 +141,9 @@ Tensor *max_pool_2d(const Tensor *input, size_t pool_size, size_t stride) {
     assert(has_batch_dim || input->n_dims == 3);
 
     size_t batch_size = has_batch_dim ? input->dims[0] : 1;
-    size_t input_channels = input->dims[0+has_batch_dim];
-    size_t input_height = input->dims[1+has_batch_dim];
-    size_t input_width = input->dims[2+has_batch_dim];
+    size_t input_channels = input->dims[0 + has_batch_dim];
+    size_t input_height = input->dims[1 + has_batch_dim];
+    size_t input_width = input->dims[2 + has_batch_dim];
 
     size_t output_height = (input_height - pool_size) / stride + 1;
     size_t output_width = (input_width - pool_size) / stride + 1;
@@ -136,30 +151,38 @@ Tensor *max_pool_2d(const Tensor *input, size_t pool_size, size_t stride) {
     size_t output_dims[] = {batch_size, input_channels, output_height, output_width};
     Tensor *output = create_tensor(4, output_dims);
 
-    for (size_t b = 0; b < batch_size; b++) {
+    size_t input_index, ii_batch_size, ii_input_channels, ii_input_height, ii_input_width;
+    size_t output_index, oi_batch_size, oi_output_channels, oi_output_height, oi_output_width;
+
+    size_t b;
+    #pragma omp parallel for private(b, input_index, ii_batch_size, ii_input_channels, ii_input_height, ii_input_width, output_index, oi_batch_size, oi_output_channels, oi_output_height, oi_output_width)
+    for (b = 0; b < batch_size; b++) {
+        oi_batch_size = b * input_channels * output_height * output_width;
+        ii_batch_size = b * input_channels * input_height * input_width;
         for (size_t i = 0; i < input_channels; i++) {
+            oi_output_channels = i * output_height * output_width;
+            ii_input_channels = i * input_height * input_width;
             for (size_t j = 0; j < output_height; j++) {
+                oi_output_height = j * output_width;
                 for (size_t k = 0; k < output_width; k++) {
-                    size_t current_output_index = get_tensor_entry_index(output, (size_t[]) {b, i, j, k});
+                    oi_output_width = k;
+                    output_index = oi_batch_size + oi_output_channels + oi_output_height + oi_output_width;
 
                     float max_value = -INFINITY;
 
                     for (size_t l = 0; l < pool_size; l++) {
+                        ii_input_height = (j * stride + l) * input_width;
                         for (size_t m = 0; m < pool_size; m++) {
-                            size_t current_input_indices[4];
+                            ii_input_width = k * stride + m;
+                            input_index = ii_batch_size + ii_input_channels + ii_input_height + ii_input_width;
 
-                            current_input_indices[0] = b;
-                            current_input_indices[0+has_batch_dim] = i;
-                            current_input_indices[1+has_batch_dim] = j * stride + l;
-                            current_input_indices[2+has_batch_dim] = k * stride + m;
-
-                            float current_value = get_tensor_entry_value(input, current_input_indices);
+                            float current_value = input->data[input_index];
 
                             max_value = max_value > current_value ? max_value : current_value;
                         }
                     }
 
-                    output->data[current_output_index] = max_value;
+                    output->data[output_index] = max_value;
                 }
             }
         }
